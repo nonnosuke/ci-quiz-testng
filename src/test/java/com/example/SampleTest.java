ab9a03cb15d12db6f061c1ab70429528fb0c11c2
nonnosuke <168043854+nonnosuke@users.noreply.github.com>
2025-10-23 11:51:30 +0900

    Change number

--- src/test/java/com/example/SampleTest.java
+++ src/test/java/com/example/SampleTest.java
@@ -6,7 +6,7 @@ import org.testng.annotations.Test;
 public class SampleTest {
     @Test
     public void verifyMath() {
-        int result = 3 + 1;
+        int result = 0 + 4;
         Assert.assertEquals(result, 4, "Math check");
     }
 }
